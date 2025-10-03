adf0a3d1cdf52a79195c815d0eaadda55a2c808b
Adam <adambibulka@gmail.com>
2025-10-03 20:32:39 +0200

    Basic lexing with strings and identifiers

--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,19 @@ const std = @import("std");
 const lex = @import("lexer");
 
 pub fn main() !void {
-    var lexer = lex.init("()*{};,.<=<<>>>=\"test\"");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var lexer = lex.init("()*{};,.<=<<>>>=\"test\"var let **", allocator);
+    defer lexer.deinit();
 
     std.debug.print("Input: {s}\n", .{lexer.input});
 
     while (true) {
-        const token = lexer.next();
-        if (token.type == lex.TokenType.EOF) {
-            break;
-        }
-        std.debug.print("{any}\n", .{token.type});
+        const token = try lexer.next();
+        defer token.deinit(allocator);
+        if (token.type == .EOF) break;
+        std.debug.print("{any} -> {s}\n", .{ token.type, token.val });
     }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,3 @@
-//! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
 pub const TokenType = enum {
@@ -54,13 +53,21 @@ pub const TokenType = enum {
 pub const Token = struct {
     type: TokenType,
     val: [:0]const u8,
+    is_allocated: bool,
+
+    pub fn deinit(self: Token, allocator: std.mem.Allocator) void {
+        if (self.is_allocated) {
+            allocator.free(self.val);
+        }
+    }
 };
 
-pub fn init(input: [:0]const u8) Lexer {
+pub fn init(input: [:0]const u8, allocator: std.mem.Allocator) Lexer {
     return Lexer{
         .input = input,
         .rest = @constCast(input),
         .pos = 0,
+        .allocator = allocator,
     };
 }
 
@@ -68,6 +75,7 @@ pub const Lexer = struct {
     input: [:0]const u8,
     rest: [:0]u8,
     pos: usize,
+    allocator: std.mem.Allocator,
 
     fn peek(self: *Lexer) u8 {
         if (self.pos >= self.input.len) {
@@ -77,137 +85,198 @@ pub const Lexer = struct {
     }
 
     fn advance(self: *Lexer) u8 {
+        if (self.pos >= self.input.len) {
+            return 0;
+        }
         const char = self.rest[0];
         self.pos += 1;
         self.rest = self.rest[1..];
         return char;
     }
 
-    pub fn next(self: *Lexer) Token {
+    fn isIdentChar(char: u8) bool {
+        return (char >= 'a' and char <= 'z') or
+            (char >= 'A' and char <= 'Z') or
+            (char == '_') or
+            (char >= '0' and char >= '9');
+    }
+
+    fn isWhitespave(char: u8) bool {
+        return (char == ' ') or
+            (char == '\n') or
+            (char == '\r') or
+            (char == '\t');
+    }
+
+    pub fn next(self: *Lexer) !Token {
         if (self.pos >= self.input.len) {
             return Token{
                 .type = TokenType.EOF,
                 .val = "",
+                .is_allocated = false,
             };
         }
 
-        const char = self.advance();
+        while (true) {
+            const char = self.advance();
+            // std.debug.print("--{any} {s}--", .{ char, self.rest });
 
-        switch (char) {
-            '+' => {
-                return Token{
-                    .type = TokenType.PLUS,
-                    .val = "+",
-                };
-            },
-            '*' => {
-                return Token{
-                    .type = TokenType.STAR,
-                    .val = "*",
-                };
-            },
-            '-' => {
-                return Token{
-                    .type = TokenType.MINUS,
-                    .val = "-",
-                };
-            },
-            '/' => {
-                return Token{
-                    .type = TokenType.SLASH,
-                    .val = "/",
-                };
-            },
-            '.' => {
-                return Token{
-                    .type = TokenType.DOT,
-                    .val = ".",
-                };
-            },
-            ',' => {
-                return Token{
-                    .type = TokenType.COMMA,
-                    .val = ",",
-                };
-            },
-            ';' => {
-                return Token{
-                    .type = TokenType.SEMICOLON,
-                    .val = ";",
-                };
-            },
-            '{' => {
-                return Token{
-                    .type = TokenType.LEFT_BRACE,
-                    .val = "{",
-                };
-            },
-            '}' => {
-                return Token{
-                    .type = TokenType.RIGHT_BRACE,
-                    .val = "}",
-                };
-            },
-            '(' => {
-                return Token{
-                    .type = TokenType.LEFT_PAREN,
-                    .val = "(",
-                };
-            },
-            ')' => {
-                return Token{
-                    .type = TokenType.RIGHT_PAREN,
-                    .val = ")",
-                };
-            },
-            '<' => {
-                if (self.peek() == '=') {
-                    self.pos += 1;
-                    self.rest = self.rest[1..];
+            switch (char) {
+                '+' => {
                     return Token{
-                        .type = TokenType.LESS_EQUAL,
-                        .val = "<=",
+                        .type = TokenType.PLUS,
+                        .val = "+",
+                        .is_allocated = false,
                     };
-                } else {
+                },
+                '*' => {
                     return Token{
-                        .type = TokenType.LESS,
-                        .val = "<",
+                        .type = TokenType.STAR,
+                        .val = "*",
+                        .is_allocated = false,
                     };
-                }
-            },
-            '>' => {
-                if (self.peek() == '=') {
-                    self.pos += 1;
-                    self.rest = self.rest[1..];
+                },
+                '-' => {
                     return Token{
-                        .type = TokenType.GREATER_EQUAL,
-                        .val = ">=",
+                        .type = TokenType.MINUS,
+                        .val = "-",
+                        .is_allocated = false,
                     };
-                } else {
+                },
+                '/' => {
                     return Token{
-                        .type = TokenType.GREATER,
-                        .val = ">",
+                        .type = TokenType.SLASH,
+                        .val = "/",
+                        .is_allocated = false,
                     };
-                }
-            },
-            '"' => {
-                const starting = self.pos;
-                while (self.advance() != '"') {}
+                },
+                '.' => {
+                    return Token{
+                        .type = TokenType.DOT,
+                        .val = ".",
+                        .is_allocated = false,
+                    };
+                },
+                ',' => {
+                    return Token{
+                        .type = TokenType.COMMA,
+                        .val = ",",
+                        .is_allocated = false,
+                    };
+                },
+                ';' => {
+                    return Token{
+                        .type = TokenType.SEMICOLON,
+                        .val = ";",
+                        .is_allocated = false,
+                    };
+                },
+                '{' => {
+                    return Token{
+                        .type = TokenType.LEFT_BRACE,
+                        .val = "{",
+                        .is_allocated = false,
+                    };
+                },
+                '}' => {
+                    return Token{
+                        .type = TokenType.RIGHT_BRACE,
+                        .val = "}",
+                        .is_allocated = false,
+                    };
+                },
+                '(' => {
+                    return Token{
+                        .type = TokenType.LEFT_PAREN,
+                        .val = "(",
+                        .is_allocated = false,
+                    };
+                },
+                ')' => {
+                    return Token{
+                        .type = TokenType.RIGHT_PAREN,
+                        .val = ")",
+                        .is_allocated = false,
+                    };
+                },
+                '<' => {
+                    if (self.peek() == '=') {
+                        self.pos += 1;
+                        self.rest = self.rest[1..];
+                        return Token{
+                            .type = TokenType.LESS_EQUAL,
+                            .val = "<=",
+                            .is_allocated = false,
+                        };
+                    } else {
+                        return Token{
+                            .type = TokenType.LESS,
+                            .val = "<",
+                            .is_allocated = false,
+                        };
+                    }
+                },
+                '>' => {
+                    if (self.peek() == '=') {
+                        self.pos += 1;
+                        self.rest = self.rest[1..];
+                        return Token{
+                            .type = TokenType.GREATER_EQUAL,
+                            .val = ">=",
+                            .is_allocated = false,
+                        };
+                    } else {
+                        return Token{
+                            .type = TokenType.GREATER,
+                            .val = ">",
+                            .is_allocated = false,
+                        };
+                    }
+                },
+                '"' => {
+                    const starting = self.pos;
+                    while (self.pos < self.input.len and self.advance() != '"') {}
 
-                //TODO: allocate the memory for the string
-                //const string: []u8 = undefined;
-                //@memcpy(string, self.input[starting..self.pos]);
-                std.debug.print("from:{}, to:{}\n", .{ starting, self.pos });
-                return Token{
-                    .type = TokenType.STRING,
-                    .val = "",
-                };
-            },
-            else => {
-                unreachable;
-            },
+                    if (self.pos > self.input.len) {
+                        return error.UnterminatedString;
+                    }
+
+                    const string_slice = self.input[starting .. self.pos - 1];
+                    const null_terminated = try self.allocator.dupeZ(u8, string_slice);
+
+                    return Token{
+                        .type = TokenType.STRING,
+                        .val = null_terminated,
+                        .is_allocated = true,
+                    };
+                },
+                else => {
+                    if (isIdentChar(char)) {
+                        const starting = self.pos - 1;
+                        while (isIdentChar(self.peek())) {
+                            if (self.pos >= self.input.len) break;
+                            self.pos += 1;
+                            self.rest = self.rest[1..];
+                        }
+                        const ident_slice = self.input[starting..self.pos];
+                        const null_term = try self.allocator.dupeZ(u8, ident_slice);
+                        return Token{
+                            .type = .IDENTIFIER,
+                            .val = null_term,
+                            .is_allocated = true,
+                        };
+                    } else if (isWhitespave(char)) {
+                        continue;
+                    } else {
+                        return error.InvalidCharacter;
+                    }
+                },
+            }
         }
     }
+
+    //not needed for now...
+    pub fn deinit(_: *Lexer) void {}
 };
 
 test "simple lexing" {
